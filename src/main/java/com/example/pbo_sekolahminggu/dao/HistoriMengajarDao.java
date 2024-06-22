0310ada868f08e38892e6b59728a81c99f821cd1
Kelly <88363184+Waxomoly@users.noreply.github.com>
2024-06-22 22:09:41 +0700

    dao and beans changed

--- src/main/java/com/example/pbo_sekolahminggu/dao/HistoriMengajarDao.java
+++ src/main/java/com/example/pbo_sekolahminggu/dao/HistoriMengajarDao.java
@@ -16,7 +16,34 @@ public class HistoriMengajarDao {
         PreparedStatement ps = null;
         ResultSet rs = null;
         // select id, nama_guru, kelas
-        String query = "select * from tbl_historiMengajar where status_aktif = 1";
+        String query = "SELECT\n" +
+                "kg.id,\n" +
+                "(SELECT nama FROM tbl_guru g WHERE g.id = kg.id_histori_mengajar),\n" +
+                "(SELECT nip FROM tbl_guru g WHERE g.id = kg.id_histori_mengajar),\n" +
+                "(SELECT \n" +
+                " \tnama_kelas FROM tbl_kelas k WHERE k.id = \n" +
+                " \t(SELECT id_kelas FROM tbl_kelas_per_tahun kpt WHERE kpt.id =\n" +
+                "\t\t(SELECT id_kelas_per_tahun FROM tbl_histori_mengajar hm WHERE hm.id =\n" +
+                "\t\t\tkg.id_histori_mengajar\n" +
+                "\t\t)\n" +
+                "\t)\n" +
+                ")\n" +
+                "|| ' ' ||\n" +
+                "COALESCE(\n" +
+                "\t(SELECT kelas_paralel FROM tbl_kelas_per_tahun kpt WHERE kpt.id =\n" +
+                "\t\t(SELECT id_kelas_per_tahun FROM tbl_histori_mengajar hm WHERE hm.id =\n" +
+                "\t\t\tkg.id_histori_mengajar\n" +
+                "\t\t)\n" +
+                "\t),''\n" +
+                ") AS kelas\n" +
+                ",\n" +
+                "(SELECT jenis_kebaktian FROM tbl_kebaktian k WHERE k.id = kg.id_kebaktian),\n" +
+                "(SELECT tanggal FROM tbl_kebaktian k WHERE k.id = kg.id_kebaktian),\n" +
+                "presensi,\n" +
+                "id_histori_mengajar,\n" +
+                "id_kebaktian\n" +
+                "FROM tbl_kehadiran_guru kg\n" +
+                "WHERE kg.status_aktif = 1";
         ArrayList<HistoriMengajar> listhistoriMengajar = new ArrayList<>();
         try {
             ps = con.prepareStatement(query);
@@ -24,9 +51,12 @@ public class HistoriMengajarDao {
             while (rs.next()) {
                 HistoriMengajar historiMengajar = new HistoriMengajar();
                 historiMengajar.setID_HISTORI_MENGAJAR(rs.getInt("id"));
-                // historiMengajar.set nama_guru
-                // historiMengajar.set kelas
-
+                historiMengajar.setNamaGuru(rs.getString("nama"));
+                historiMengajar.setNip(rs.getString("nip"));
+                historiMengajar.setKelas(rs.getString("kelas"));
+                historiMengajar.setTahunAjaran(rs.getString("tahun_ajaran"));
+                historiMengajar.setID_GURU(rs.getInt("id_guru"));
+                historiMengajar.setID_KELAS_PER_TAHUN(rs.getInt("id_kelas_per_tahun"));
                 listhistoriMengajar.add(historiMengajar);
             }
         } catch (SQLException e) {
@@ -40,12 +70,12 @@ public class HistoriMengajarDao {
     // SAVE
     public static void save(Connection con, HistoriMengajar historiMengajar) {
         PreparedStatement statement = null;
-        String query = "INSERT INTO tbl_historiMengajar (kelas, nama_guru) VALUES (?, ?, ?)";
+        String query = "INSERT INTO tbl_histori_mengajar (id_kelas_per_tahun, id_guru) VALUES (?, ?)";
 
         try {
             statement = con.prepareStatement(query);
-//            statement.setString(2, historiMengajar. ); // kelas
-//            statement.setString(2, historiMengajar. ); // nama_guru
+            statement.setInt(1, historiMengajar.getID_KELAS_PER_TAHUN() ); // kelas
+            statement.setInt(2, historiMengajar.getID_GURU()); // guru
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Error saving historiMengajar: " + e.getMessage());
@@ -58,12 +88,12 @@ public class HistoriMengajarDao {
     // EDIT
     public static void edit(Connection con, HistoriMengajar historiMengajar) {
         PreparedStatement statement = null;
-        String query = "UPDATE tbl_historiMengajar SET kelas = ?, nama_guru = ? WHERE id = ?";
+        String query = "UPDATE tbl_histori_mengajar SET id_kelas_per_tahun = ?, id_guru = ? WHERE id = ?";
 
         try {
             statement = con.prepareStatement(query);
-//            statement.setString(1, historiMengajar.); // get kelas
-//            statement.setString(2, historiMengajar.); // get nama_guru
+            statement.setInt(1, historiMengajar.getID_KELAS_PER_TAHUN());
+            statement.setInt(2, historiMengajar.getID_GURU());
             statement.setInt(3, historiMengajar.getID_HISTORI_MENGAJAR());
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -76,7 +106,7 @@ public class HistoriMengajarDao {
     // DELETE
     public static void delete(Connection con, HistoriMengajar historiMengajar) {
         PreparedStatement statement = null;
-        String query = "UPDATE tbl_historiMengajar SET status_aktif = 0 WHERE id = ?";
+        String query = "UPDATE tbl_histori_mengajar SET status_aktif = 0 WHERE id = ?";
 
         try {
             statement = con.prepareStatement(query);
--- src/main/java/com/example/pbo_sekolahminggu/dao/KehadiranGuruDao.java
+++ src/main/java/com/example/pbo_sekolahminggu/dao/KehadiranGuruDao.java
@@ -13,7 +13,34 @@ public class KehadiranGuruDao {
     public static ArrayList<KehadiranGuru> getAll(Connection con) {
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String query = "select * from tbl_kehadiranGuru where status_aktif = 1";
+        String query = "SELECT\n" +
+                "kg.id,\n" +
+                "(SELECT nama FROM tbl_guru g WHERE g.id = kg.id_histori_mengajar),\n" +
+                "(SELECT nip FROM tbl_guru g WHERE g.id = kg.id_histori_mengajar),\n" +
+                "(SELECT \n" +
+                " \tnama_kelas FROM tbl_kelas k WHERE k.id = \n" +
+                " \t(SELECT id_kelas FROM tbl_kelas_per_tahun kpt WHERE kpt.id =\n" +
+                "\t\t(SELECT id_kelas_per_tahun FROM tbl_histori_mengajar hm WHERE hm.id =\n" +
+                "\t\t\tkg.id_histori_mengajar\n" +
+                "\t\t)\n" +
+                "\t)\n" +
+                ")\n" +
+                "|| ' ' ||\n" +
+                "COALESCE(\n" +
+                "\t(SELECT kelas_paralel FROM tbl_kelas_per_tahun kpt WHERE kpt.id =\n" +
+                "\t\t(SELECT id_kelas_per_tahun FROM tbl_histori_mengajar hm WHERE hm.id =\n" +
+                "\t\t\tkg.id_histori_mengajar\n" +
+                "\t\t)\n" +
+                "\t),''\n" +
+                ") AS kelas\n" +
+                ",\n" +
+                "(SELECT jenis_kebaktian FROM tbl_kebaktian k WHERE k.id = kg.id_kebaktian),\n" +
+                "(SELECT tanggal FROM tbl_kebaktian k WHERE k.id = kg.id_kebaktian),\n" +
+                "presensi,\n" +
+                "id_histori_mengajar,\n" +
+                "id_kebaktian\n" +
+                "FROM tbl_kehadiran_guru kg\n" +
+                "WHERE kg.status_aktif = 1\n";
         ArrayList<KehadiranGuru> listkehadiranGuru = new ArrayList<>();
         try {
             ps = con.prepareStatement(query);
@@ -22,6 +49,13 @@ public class KehadiranGuruDao {
                 KehadiranGuru kehadiranGuru = new KehadiranGuru();
                 kehadiranGuru.setID_KEHADIRAN_GURU(rs.getInt("id"));
                 kehadiranGuru.setPresensi(rs.getBoolean("presensi"));
+                kehadiranGuru.setNama(rs.getString("nama"));
+                kehadiranGuru.setNip(rs.getString("nip"));
+                kehadiranGuru.setJenisKebaktian(rs.getString("jenis_kebaktian"));
+                kehadiranGuru.setTanggal(rs.getDate("tanggal"));
+                kehadiranGuru.setID_KEBAKTIAN(rs.getInt("id_kebaktian"));
+                kehadiranGuru.setID_HISTORI_MENGAJAR(rs.getInt("id_histori_mengajar"));
+                kehadiranGuru.setKelas(rs.getString("kelas"));
                 listkehadiranGuru.add(kehadiranGuru);
             }
         } catch (SQLException e) {
@@ -36,11 +70,13 @@ public class KehadiranGuruDao {
     // SAVE
     public static void save(Connection con, KehadiranGuru kehadiranGuru) {
         PreparedStatement statement = null;
-        String query = "INSERT INTO tbl_kehadiranGuru (presensi) VALUES (?)";
+        String query = "INSERT INTO tbl_kehadiran_guru (id_histori_mengajar, id_kebaktian, presensi) VALUES (?, ?, ?)";
 
         try {
             statement = con.prepareStatement(query);
-            statement.setBoolean(1, kehadiranGuru.isPresensi());
+            statement.setInt(1, kehadiranGuru.getID_HISTORI_MENGAJAR());
+            statement.setInt(2, kehadiranGuru.getID_KEBAKTIAN());
+            statement.setBoolean(3, kehadiranGuru.isPresensi());
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Error saving kehadiranGuru: " + e.getMessage());
@@ -52,12 +88,14 @@ public class KehadiranGuruDao {
     // EDIT
     public static void edit(Connection con, KehadiranGuru kehadiranGuru) {
         PreparedStatement statement = null;
-        String query = "UPDATE tbl_kehadiranGuru SET presensi = ? WHERE id = ?";
+        String query = "UPDATE tbl_kehadiran_guru SET id_histori_mengajar=?, id_kebaktian=?, presensi = ? WHERE id = ?";
 
         try {
             statement = con.prepareStatement(query);
-            statement.setBoolean(1, kehadiranGuru.isPresensi());
-            statement.setInt(2, kehadiranGuru.getID_KEHADIRAN_GURU());
+            statement.setInt(1, kehadiranGuru.getID_HISTORI_MENGAJAR());
+            statement.setInt(2, kehadiranGuru.getID_KEBAKTIAN());
+            statement.setBoolean(3, kehadiranGuru.isPresensi());
+            statement.setInt(4, kehadiranGuru.getID_KEHADIRAN_GURU());
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Error editing kehadiranGuru: " + e.getMessage());
@@ -69,7 +107,7 @@ public class KehadiranGuruDao {
     // DELETE
     public static void delete(Connection con, KehadiranGuru kehadiranGuru) {
         PreparedStatement statement = null;
-        String query = "UPDATE tbl_kehadiranGuru SET status_aktif = 0 WHERE id = ?";
+        String query = "UPDATE tbl_kehadiran_guru SET status_aktif = 0 WHERE id = ?";
 
         try {
             statement = con.prepareStatement(query);
